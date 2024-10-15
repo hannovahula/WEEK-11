68532b0263c9494cd9a3681e055af0574ea41119
Hanno Vah <hanno.vahula@tktk.ee>
2024-10-16 00:44:19 +0300

    week11 dotenv

---
+++ db.js
@@ -0,0 +1,26 @@
+const dotenv = require(`dotenv`);
+dotenv.config();
+const { Pool } = require(`pg`);
+
+
+const itemsPool = new Pool ({
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+
+
+
+// const itemsPool = new Pool({
+
+//     user: 'postgres',
+//     password: 'Albert27H',
+//     host: 'localhost',
+//     port: 5432,
+//     database: `myrecipes`
+// });
+
+module.exports = itemsPool;
